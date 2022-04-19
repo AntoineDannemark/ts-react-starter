45456a03a81f6d4e00e5f41c8134fdf49b7977ed
Antoine <antoine.dannemark@protonmail.com>
2022-04-19 23:11:00 +0200

    only client module from socketio + test @app's useeffect

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { io } from 'socket.io-client';
 
 import Board from '../features/board/Board';
 
-const App: React.FC = () => (
-    <>
-        <Board />
-    </>
-);
+const App: React.FC = () => {
+    useEffect(() => {
+        const socket = io('http://127.0.0.1:5000');
+
+        // eslint-disable-next-line no-console
+        console.log(socket);
+    }, []);
+
+    return (
+        <>
+            <Board />
+        </>
+    );
+};
 
 export default App;
